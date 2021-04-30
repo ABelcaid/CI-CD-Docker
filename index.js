5104a18ffdd353d8110a0d3acddfcc6c547ccaef
belcaid abderrahim <belcaidna@gmail.com>
2021-04-30 15:04:54 +0000

    edit wokrflow3

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
  });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening on http://localhost:${port}`)
 })
 
 module.exports = app
