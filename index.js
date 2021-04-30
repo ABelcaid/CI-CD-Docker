5f73006baae6052d7a00f2a9030ff50dbc7e7f55
belcaid abderrahim <belcaidna@gmail.com>
2021-04-30 14:03:29 +0000

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+const data =  require("./data.json");
+
+
+app.get("/", (req, res) => {
+    res.status(200).json(data);
+   
+
+ });
+
+ app.get('/:id',(req,res)=>{
+
+        const id =req.params.id;
+        const User = data.find(user => user.id === id);
+        res.json(User)
+  
+
+
+ });
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+module.exports = app
---
+++ test/index.js
@@ -0,0 +1,37 @@
+let app = require("../index");
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+
+chai.should();
+chai.use(chaiHttp);
+
+const id = 5;
+
+
+describe("API Tests", () => {
+  describe("Test GET All Data", () => {
+    it("It should return all data", (done) => {
+      chai
+        .request(app)
+        .get("/")
+        .end((err, response) => {
+          response.body.should.be.a("array");
+          response.body.length.should.not.be.eq(0);
+          done();
+        });
+    });
+  });
+
+  describe("Test GET User By Id", () => {
+      it("It should return right item", (done) => {
+        chai
+          .request(app)
+          .get("/" + id)
+          .end((err, response) => {
+            response.body.should.be.a("object");
+            done();
+          });
+      });
+    });
+
+});
